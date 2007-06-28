88fa248b0c8059e4b57960af1aad14a1a1500fbc
Hannes Wyss <hwyss@ywesee.com>
2007-06-28 09:19:51 +0200

    Refactoring to make the widget generic: InfoToggler -> ContentToggler

--- test/selenium/test_login.rb
+++ test/selenium/test_login.rb
@@ -10,68 +10,68 @@ module BBMB
 class TestLogin < Test::Unit::TestCase
   include Selenium::TestCase
   def test_login
-    @selenium.open "/"
-    assert_equal "BBMB", @selenium.get_title
-    assert @selenium.is_text_present("Wilkommen bei V")
-    assert_equal "Email", @selenium.get_text("//label[@for='email']")
-    assert @selenium.is_element_present("email")
-    assert_equal "Passwort", @selenium.get_text("//label[@for='pass']")
-    assert @selenium.is_element_present("pass")
+    open "/"
+    assert_equal "BBMB", get_title
+    assert is_text_present("Wilkommen bei V")
+    assert_equal "Email", get_text("//label[@for='email']")
+    assert is_element_present("email")
+    assert_equal "Passwort", get_text("//label[@for='pass']")
+    assert is_element_present("pass")
     assert_match Regexp.new(BBMB.config.http_server), 
-      @selenium.get_attribute("//form[@name='login']@action")
-    assert @selenium.is_element_present("//input[@name='login']")
+      get_attribute("//form[@name='login']@action")
+    assert is_element_present("//input[@name='login']")
   end
   def test_login__fail_unknown_user
-    @selenium.open "/"
-    assert_equal "BBMB", @selenium.get_title
+    open "/"
+    assert_equal "BBMB", get_title
     @auth.should_receive(:login).and_return { raise Yus::UnknownEntityError }
-    @selenium.type "email", "unknown@bbmb.ch"
-    @selenium.type "pass", "secret"
-    @selenium.click "//input[@type='submit']"
-    @selenium.wait_for_page_to_load "30000"
-    assert_equal "BBMB", @selenium.get_title
-    assert_equal "error", @selenium.get_attribute("//label[@for='email']@class")
+    type "email", "unknown@bbmb.ch"
+    type "pass", "secret"
+    click "//input[@type='submit']"
+    wait_for_page_to_load "30000"
+    assert_equal "BBMB", get_title
+    assert_equal "error", get_attribute("//label[@for='email']@class")
   end
   def test_login__fail_wrong_password
-    @selenium.open "/"
-    assert_equal "BBMB", @selenium.get_title
+    open "/"
+    assert_equal "BBMB", get_title
     @auth.should_receive(:login).and_return { raise Yus::AuthenticationError }
-    @selenium.type "email", "unknown@bbmb.ch"
-    @selenium.type "pass", "secret"
-    @selenium.click "//input[@type='submit']"
-    @selenium.wait_for_page_to_load "30000"
-    assert_equal "BBMB", @selenium.get_title
-    assert_equal "error", @selenium.get_attribute("//label[@for='pass']@class")
+    type "email", "unknown@bbmb.ch"
+    type "pass", "secret"
+    click "//input[@type='submit']"
+    wait_for_page_to_load "30000"
+    assert_equal "BBMB", get_title
+    assert_equal "error", get_attribute("//label[@for='pass']@class")
   end
   def test_login__force_home
-    @selenium.open "/"
-    assert_equal "BBMB", @selenium.get_title
-    @selenium.open "/de/home"
-    assert @selenium.is_text_present("Wilkommen bei V")
-    assert @selenium.is_element_present("email")
-    assert @selenium.is_element_present("pass")
-    assert @selenium.is_element_present("//input[@name='login']")
+    open "/"
+    assert_equal "BBMB", get_title
+    open "/de/home"
+    assert is_text_present("Wilkommen bei V")
+    assert is_element_present("email")
+    assert is_element_present("pass")
+    assert is_element_present("//input[@name='login']")
   end
   def test_logout__clean
     @auth.should_ignore_missing
     @persistence.should_receive(:all).and_return([])
     user = login_admin
-    assert @selenium.is_element_present("link=Abmelden")
-    assert_equal "Abmelden", @selenium.get_text("link=Abmelden")
-    @selenium.click "link=Abmelden"
-    @selenium.wait_for_page_to_load "30000"
-    assert_equal "BBMB", @selenium.get_title
-    assert_equal "Email", @selenium.get_text("//label[@for='email']")
-    assert @selenium.is_element_present("email")
-    assert_equal "Passwort", @selenium.get_text("//label[@for='pass']")
-    assert @selenium.is_element_present("pass")
-    @selenium.open "/de/customers"
+    assert is_element_present("link=Abmelden")
+    assert_equal "Abmelden", get_text("link=Abmelden")
+    click "link=Abmelden"
+    wait_for_page_to_load "30000"
+    assert_equal "BBMB", get_title
+    assert_equal "Email", get_text("//label[@for='email']")
+    assert is_element_present("email")
+    assert_equal "Passwort", get_text("//label[@for='pass']")
+    assert is_element_present("pass")
+    open "/de/customers"
     # session is now invalid, we stay in login-mask
-    assert_equal "BBMB", @selenium.get_title
-    assert_equal "Email", @selenium.get_text("//label[@for='email']")
-    assert @selenium.is_element_present("email")
-    assert_equal "Passwort", @selenium.get_text("//label[@for='pass']")
-    assert @selenium.is_element_present("pass")
+    assert_equal "BBMB", get_title
+    assert_equal "Email", get_text("//label[@for='email']")
+    assert is_element_present("email")
+    assert_equal "Passwort", get_text("//label[@for='pass']")
+    assert is_element_present("pass")
   end
   def test_logout__timeout
     @auth.should_ignore_missing
@@ -79,22 +79,33 @@ class TestLogin < Test::Unit::TestCase
     user = login_admin
     user.should_receive(:expired?).and_return(true)
     user.should_receive(:logout)
-    @selenium.refresh
-    @selenium.wait_for_page_to_load "30000"
-    assert_equal "BBMB", @selenium.get_title
-    assert @selenium.is_element_present("email")
-    assert @selenium.is_element_present("pass")
+    refresh
+    wait_for_page_to_load "30000"
+    assert_equal "BBMB", get_title
+    assert is_element_present("email")
+    assert is_element_present("pass")
   end
   def test_logout__disconnect
     @auth.should_ignore_missing
     @persistence.should_receive(:all).and_return([])
     user = login_admin
     user.should_receive(:expired?).and_return { raise DRb::DRbError }
-    @selenium.refresh
-    @selenium.wait_for_page_to_load "30000"
-    assert_equal "BBMB", @selenium.get_title
-    assert @selenium.is_element_present("email")
-    assert @selenium.is_element_present("pass")
+    refresh
+    wait_for_page_to_load "30000"
+    assert_equal "BBMB", get_title
+    assert is_element_present("email")
+    assert is_element_present("pass")
+  end
+  def test_new_customer_form
+    open "/"
+    assert_equal "BBMB", get_title
+    assert_equal "Neuer Kunde", get_text("//label[@for='new_customer']")
+    assert_equal <<-EOS.strip,  get_text("//a[@name='new_customer']")
+Bestellen Sie jetzt online. Wir richten für Sie den spezifisch auf Ihre Praxis zugeschnittenen, benutzerfreundlichen E-Shop ein!
+Unser Kundenservice oder unsere Aussendienstmitarbeiter beraten Sie gerne!
+    EOS
+    assert_match Regexp.new("^mailto:"), 
+                 get_attribute("//a[@name='new_customer']@href")
   end
 end
   end
